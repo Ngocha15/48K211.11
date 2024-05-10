9f0a6dacaf4e53cfb0c52017971108e260376ce1
Ngocha15 <hngocha1509@gmail.com>
2024-05-10 21:44:38 +0700

    feat: init project

---
+++ management_app/README.md
@@ -0,0 +1,3 @@
+# management_app
+
+A new Flutter project.
---
+++ management_app/android/app/src/main/kotlin/com/example/management_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.management_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
